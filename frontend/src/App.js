e0ca549a7d3bc5941e21733ce568967b57471268
JaredPeralta <90805689+JaredPeralta@users.noreply.github.com>
2023-10-24 09:49:24 -0500

    Iniciando conel front,vistas basicas,enrutamiento

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hi</p>
+    </div>
+  );
+}
+
+export default App;
